9f2821f25206503fa7f99291f3656d336cfd57da
Yuki Hattori <yukihattori1116@gmail.com>
2018-06-01 01:18:02 +0900

    Suppress confusable import in inline style by import suppress plugin

---
+++ src/postcss/import/suppress.js
@@ -0,0 +1,28 @@
+/** @module */
+import postcss from 'postcss'
+import postcssImportParse from './parse'
+
+/**
+ * Marpit PostCSS import suppress plugin.
+ *
+ * Comment out `@import` / `@import-theme` rules that have imported theme.
+ *
+ * This plugin is useful to prevent the inline style's rolled-up theme import by
+ * unexpected order.
+ *
+ * @alias module:postcss/import/suppress
+ * @param {ThemeSet} themeSet ThemeSet instance.
+ */
+const plugin = postcss.plugin('marpit-postcss-import-suppress', themeSet =>
+  postcss([
+    postcssImportParse,
+    css => {
+      css.walk(node => {
+        if (node.marpitImportParse && themeSet.has(node.marpitImportParse))
+          node.replaceWith(`${node.raw('before')}/* ${node.toString()}; */`)
+      })
+    },
+  ])
+)
+
+export default plugin
