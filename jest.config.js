8d6ad59139e2c9906f24c2a869922e2efc58696f
Yuki Hattori <yukihattori1116@gmail.com>
2019-01-30 23:26:34 +0900

    Use setupFilesAfterEnv option in latest Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
   coverageThreshold: { global: { lines: 95 } },
-  setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testRegex: '(/(test|__tests__)/(?!_).*|(\\.|/)(test|spec))\\.js$',
   testURL: 'http://localhost',
   moduleFileExtensions: ['js', 'json', 'node'],
