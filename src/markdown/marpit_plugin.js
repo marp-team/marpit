bddef4d5b5c5380085667ce79092267793ec4489
Yuki Hattori <yukihattori1116@gmail.com>
2019-04-02 10:09:04 +0900

    Skip instance type check in Marpit plugin wrapper
    Some tests using plain object as Marpit stub, so this change becomes
    easy to write unit tests.

--- src/markdown/marpit_plugin.js
+++ src/markdown/marpit_plugin.js
@@ -5,17 +5,15 @@ import Marpit from '../marpit'
  * Marpit plugin wrapper.
  *
  * Convert passed markdown-it plugin to Marpit plugin. Marpit plugin would
- * require that markdown-it instance has `marpit` member as Marpit instance.
+ * require that markdown-it instance has `marpit` member.
  *
  * @alias module:markdown/marpit_plugin
  * @returns {Function} markdown-it plugin for Marpit.
  */
 export default function marpitPlugin(plugin) {
   return (md, ...args) => {
-    if (md.marpit instanceof Marpit) return plugin.call(this, md, ...args)
+    if (md.marpit) return plugin.call(this, md, ...args)
 
-    throw new Error(
-      "Marpit's markdown-it plugin requires `marpit` member as Marpit instance."
-    )
+    throw new Error("Marpit's markdown-it plugin requires `marpit` member.")
   }
 }
