d9223b2d2ed1189161640932d84ebc1b63f262e0
Yuki Hattori <yukihattori1116@gmail.com>
2019-04-02 11:12:17 +0900

    Fix ESLint

--- src/markdown/marpit_plugin.js
+++ src/markdown/marpit_plugin.js
@@ -1,5 +1,4 @@
 /** @module */
-import Marpit from '../marpit'
 
 /**
  * Marpit plugin wrapper.
@@ -10,10 +9,12 @@ import Marpit from '../marpit'
  * @alias module:markdown/marpit_plugin
  * @returns {Function} markdown-it plugin for Marpit.
  */
-export default function marpitPlugin(plugin) {
+function marpitPlugin(plugin) {
   return (md, ...args) => {
     if (md.marpit) return plugin.call(this, md, ...args)
 
     throw new Error("Marpit's markdown-it plugin requires `marpit` member.")
   }
 }
+
+export default marpitPlugin
