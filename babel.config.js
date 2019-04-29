3df367f1fc16669067a7e1e679f53880e3421fde
Yuki Hattori <yukihattori1116@gmail.com>
2019-04-30 07:58:16 +0900

    Drop Node 6 support

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: '6.14' } }]],
+  presets: [['@babel/preset-env', { targets: { node: '8' } }]],
   plugins: [
     ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
     '@babel/plugin-proposal-class-properties',
