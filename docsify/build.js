68ae0c181bff5b2d19b91cbdec753a4695702b39
Yuki Hattori <yukihattori1116@gmail.com>
2019-05-05 16:23:31 +0900

    Remove fibers to support carbon build

--- docsify/build.js
+++ docsify/build.js
@@ -5,24 +5,23 @@ const mkdirp = require('mkdirp')
 const postcss = require('postcss')
 const path = require('path')
 const sass = require('sass')
-const fiber = require('fibers')
 
 const from = path.join(__dirname, './docsify.scss')
 const to = path.join(__dirname, '../docs/style/docsify.css')
 
-sass.render(
-  { fiber, file: from, outFile: to, sourceMap: true, sourceMapEmbed: true },
-  (err, { css }) => {
-    if (err) throw err
+const { css } = sass.renderSync({
+  file: from,
+  outFile: to,
+  sourceMap: true,
+  sourceMapEmbed: true,
+})
 
-    postcss([
-      autoprefixer,
-      cssnano({ preset: ['default', { mergeLonghand: false }] }),
-    ])
-      .process(css, { from, to, map: { annotation: false, inline: false } })
-      .then(ret => {
-        mkdirp.sync(path.dirname(to))
-        fs.writeFileSync(to, ret.css)
-      })
-  }
-)
+postcss([
+  autoprefixer,
+  cssnano({ preset: ['default', { mergeLonghand: false }] }),
+])
+  .process(css, { from, to, map: { annotation: false, inline: false } })
+  .then(ret => {
+    mkdirp.sync(path.dirname(to))
+    fs.writeFileSync(to, ret.css)
+  })
