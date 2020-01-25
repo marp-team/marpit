b3da5304dee18556f734a1d862bb9930ddecb3fe
Yuki Hattori <yukihattori1116@gmail.com>
2020-01-25 13:53:16 +0900

    Setting up default test environment as node

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   coverageThreshold: { global: { lines: 95 } },
   restoreMocks: true,
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'node',
   testRegex: '(/(test|__tests__)/(?!_).*|(\\.|/)(test|spec))\\.js$',
   testURL: 'http://localhost',
   moduleFileExtensions: ['js', 'json', 'node'],
