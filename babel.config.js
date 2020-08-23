c223217123f5de7ace3c82aa99bd1faf87ab4c61
Yuki Hattori <yukihattori1116@gmail.com>
2020-08-23 17:05:57 +0900

    Remove unnecessary packages

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   // TODO: Drop support for EOL Node <= 8 in Marpit v2
-  presets: [['@babel/preset-env', { targets: { node: '8' } }]],
-  plugins: [
-    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }],
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-private-methods',
+  presets: [
+    ['@babel/preset-env', { targets: { node: '8' }, shippedProposals: true }],
   ],
 }
