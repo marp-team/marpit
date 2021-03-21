b0232c28c846ee5e76c4aaf19865078b1c525201
Yuki Hattori <yukihattori1116@gmail.com>
2021-03-22 00:57:16 +0900

    Prevent using imported PostCSS instance in plugins

--- src/postcss/import/replace.js
+++ src/postcss/import/replace.js
@@ -1,5 +1,4 @@
 /** @module */
-import postcss from 'postcss'
 import postcssPlugin from '../../helpers/postcss_plugin'
 import postcssImportParse from './parse'
 
@@ -15,37 +14,44 @@ import postcssImportParse from './parse'
  * @param {ThemeSet} themeSet ThemeSet instance.
  */
 const plugin = (themeSet, importedThemes = []) =>
-  postcss([
-    postcssImportParse,
-    postcssPlugin('marpit-postcss-import-replace', () => (css) => {
-      const prepends = []
+  postcssPlugin('marpit-postcss-import-replace', () => ({
+    plugins: [
+      postcssImportParse(),
+      postcssPlugin(
+        'marpit-postcss-import-replace-processor',
+        () => (css, { postcss }) => {
+          const prepends = []
 
-      css.walk((node) => {
-        const name = node.marpitImportParse
+          css.walk((node) => {
+            const name = node.marpitImportParse
 
-        if (name) {
-          const theme = themeSet.get(name)
+            if (name) {
+              const theme = themeSet.get(name)
 
-          if (theme) {
-            if (importedThemes.includes(name))
-              throw new Error(`Circular "${name}" theme import is detected.`)
+              if (theme) {
+                if (importedThemes.includes(name))
+                  throw new Error(
+                    `Circular "${name}" theme import is detected.`
+                  )
 
-            const processed = postcss([
-              plugin(themeSet, [...importedThemes, name]),
-            ]).process(theme.css)
+                const processed = postcss([
+                  plugin(themeSet, [...importedThemes, name]),
+                ]).process(theme.css)
 
-            if (node.name === 'import') {
-              node.replaceWith(processed.root)
-            } else {
-              node.remove()
-              prepends.unshift(processed.root)
+                if (node.name === 'import') {
+                  node.replaceWith(processed.root)
+                } else {
+                  node.remove()
+                  prepends.unshift(processed.root)
+                }
+              }
             }
-          }
-        }
-      })
+          })
 
-      for (const root of prepends) css.first.before(root)
-    }),
-  ])
+          for (const root of prepends) css.first.before(root)
+        }
+      )(),
+    ],
+  }))
 
 export default plugin
