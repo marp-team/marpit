206dbd4fb60ba8026faa164b8fdfc151a20abe02
Yuki Hattori <yukihattori1116@gmail.com>
2022-05-21 14:25:33 +0900

    Match specificity of the replaced :root selector with original

--- test/postcss/root/increasing_specificity.js
+++ test/postcss/root/increasing_specificity.js
@@ -10,12 +10,20 @@ describe('Marpit PostCSS root increasing specificity plugin', () => {
       from: undefined,
     })
 
-  it('replaces specific pseudo-class into ":not(a)" to increase specificity', () => {
-    expect(run(`section${pseudoClass} {}`).css).toBe('section:not(a) {}')
+  it('replaces specific pseudo-class into ":where(section):not([\\20 root])" to increase specificity', () => {
+    expect(run(`section${pseudoClass} {}`).css).toBe(
+      'section:where(section):not([\\20 root]) {}'
+    )
 
     // With replaced :root selector via root replace plugin
-    expect(run(`:root {}`).css).toBe('section:not(a) {}')
-    expect(run(`section :root {}`).css).toBe('section section:not(a) {}')
-    expect(run(`:root.klass div {}`).css).toBe('section:not(a).klass div {}')
+    expect(run(`:root {}`).css).toBe(
+      'section:where(section):not([\\20 root]) {}'
+    )
+    expect(run(`section :root {}`).css).toBe(
+      'section section:where(section):not([\\20 root]) {}'
+    )
+    expect(run(`:root.klass div {}`).css).toBe(
+      'section:where(section):not([\\20 root]).klass div {}'
+    )
   })
 })
