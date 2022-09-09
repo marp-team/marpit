8545847e6fe01df9239cbcee3edbaa59df4c7847
Yuki Hattori <yukihattori1116@gmail.com>
2022-09-10 03:20:12 +0900

    Add a test for plugin interface

---
+++ test/plugin.js
@@ -0,0 +1,36 @@
+import markdownIt from 'markdown-it'
+import pluginAsDefaultExport from '../plugin'
+import { marpitPlugin } from '../plugin'
+import { Marpit } from '../src/index'
+
+describe('Plugin interface', () => {
+  it('is compatible as CommonJS module', () => {
+    expect(require('../plugin')).toBeInstanceOf(Function)
+  })
+
+  it('is compatible as ES Modules and able to use through default export', () => {
+    expect(pluginAsDefaultExport).toBeInstanceOf(Function)
+  })
+
+  it('is compatible as ES Modules and able to use through named export', () => {
+    expect(marpitPlugin).toBeInstanceOf(Function)
+  })
+
+  it('generates plugin function that is able to use in Marpit instance', () => {
+    const pluginFn = jest.fn((md) => md)
+    const plugin = marpitPlugin(pluginFn)
+    expect(plugin).toBeInstanceOf(Function)
+
+    const marpit = new Marpit().use(plugin)
+    expect(pluginFn).toHaveLastReturnedWith(marpit.markdown)
+    expect(marpit.markdown.marpit).toStrictEqual(marpit)
+  })
+
+  it('throws error if a generated plugin was used in markdown-it instance', () => {
+    const plugin = marpitPlugin(jest.fn())
+
+    expect(() => new markdownIt().use(plugin)).toThrowError(
+      'Marpit plugin has detected incompatible markdown-it instance.'
+    )
+  })
+})
