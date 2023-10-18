1a411cba552852cd8bf7487cce4233788b907e1f
Yuki Hattori <yukihattori1116@gmail.com>
2023-10-19 03:21:35 +0900

    Drop support against end-of-lifed Node.js versions

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // TODO: Drop support for EOL Node in Marpit v3
   presets: [
-    ['@babel/preset-env', { targets: { node: '10' }, shippedProposals: true }],
+    ['@babel/preset-env', { targets: { node: '18' }, shippedProposals: true }],
   ],
 }
